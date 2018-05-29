6d5ad8f115617b75fe235beb1b3137a2f0ad1e68
Antoine Panau <a.panau93@gmail.com>
2018-05-29 16:12:57 +0200

    mini change

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,13 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  def selection
+
+  end
+end
