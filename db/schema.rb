1acf5bfd5965ad75def1b1871cb3d00a90f4ab6a
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-29 12:08:17 +0200

    create School model and associations with School

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_094417) do
+ActiveRecord::Schema.define(version: 2018_05_29_100015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "primaires", force: :cascade do |t|
+    t.string "address"
+    t.integer "total_fees"
+    t.bigint "school_id"
+    t.text "sections", default: [], array: true
+    t.text "options", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["school_id"], name: "index_primaires_on_school_id"
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
@@ -40,4 +51,5 @@ ActiveRecord::Schema.define(version: 2018_05_29_094417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "primaires", "schools"
 end
