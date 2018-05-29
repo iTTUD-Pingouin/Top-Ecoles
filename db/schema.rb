dc26331992bdd3f8877997061f45ca4037d768d0
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-29 19:11:30 +0200

    start parsing csv to create schools in seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_122600) do
+ActiveRecord::Schema.define(version: 2018_05_29_165435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_122600) do
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
-    t.string "uai"
+    t.string "commune"
     t.string "statut"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
