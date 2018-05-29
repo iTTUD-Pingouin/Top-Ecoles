6a466ff3a7851d024c9442515400dc90190b63c1
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-29 11:46:21 +0200

    add model School

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_093642) do
+ActiveRecord::Schema.define(version: 2018_05_29_094417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "uai"
+    t.string "statut"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
