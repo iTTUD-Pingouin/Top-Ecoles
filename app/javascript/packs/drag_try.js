cde05ef7f9148a1f8bbdc918b4496e6635296e07
Guillaume Dutti <guillaume.dutti@live.fr>
2018-06-07 11:22:24 +0200

    adding drag and drop behavior

--- app/javascript/packs/drag_try.js
+++ app/javascript/packs/drag_try.js
@@ -3,7 +3,7 @@
 function drag () {
   window.Sortable = require('sortablejs')
   const simpleList = document.getElementById("simpleList")
-  Sortable.create(simpleList);
+  Sortable.create(simpleList, { animation: 450});
 };
 
 // Simple list
