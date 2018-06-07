1cc38d22a4bc35e056e506b3efb13d47b8ba6d3f
Guillaume Dutti <guillaume.dutti@live.fr>
2018-06-07 11:03:10 +0200

    add drag and drop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import { clickToActiveCardButton } from './click_to_active_card_button.js';
 import { moveChevrons } from './chevrons_filters_movements.js';
 import { clickToEnSavoirPlus } from './click_to_savoir_plus.js';
 import { selectFilter } from './select_filter.js';
+import { drag } from './drag_try.js';
+
 
 
 clickToSubmit();
@@ -15,3 +17,6 @@ selectFilter();
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+
+
+drag();
