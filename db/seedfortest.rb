8c127baf91cce23875c7141f0ff16a1754d9a998
Guillaume Dutti <guillaume.dutti@live.fr>
2018-06-25 15:50:19 +0200

    implement parsing logic for lyceecollegefrance csv file in test seed

--- db/seedfortest.rb
+++ db/seedfortest.rb
@@ -4,7 +4,7 @@ require 'csv'
 filepath = 'db/fr-en-annuaire-education.csv'
 csv_options = {col_sep: ';', headers: :first_row }
 
-
+# PARSING FIRST FILE = FR-annuaire-education ------------------------------------------------------------------------------------------
 puts "Parsing fr-en-annuaire-education csv file ..."
 
 CSV.foreach(filepath, csv_options) do |row|
@@ -126,11 +126,139 @@ CSV.foreach(filepath, csv_options) do |row|
     # pensionnat = [true, false].sample
     # college.pensionnat = pensionnat
     # college.rating = (1..5).to_a.sample
+  puts "Création des Lycées ..."
 
-    college.save!
   elsif row["type d'établissement"] == "Lycée"
-    lycee = Lycee.create!(address: row["adresse"], school_id: school.id, statut: school.statut, uai: row['code UAI'], commune: row['commune'])
-    lycee.rating = (1..5).to_a.sample
+    lycee = Lycee.create!(
+      address: row["Adresse_1"],
+      school_id: school.id,
+      statut: school.statut,
+      uai: row['Identifiant_de_l_etablissement'],
+      commune: row['Nom_commune'],
+      name: row['Nom_etablissement'],
+      cp: row['Code postal'],
+      code_commune: row['Code_commune'],
+      code_departement: row['Code_departement'],
+      telephone: row['Telephone'],
+      website: row['Web'],
+      students_number: row['Nombre_d_eleves'],
+      onisep_view: row['Fiche_onisep']
+      )
+
+    # Filières, si présente, paramètre passé à true
+
+    if row['Restauration'] == 1
+      college.has_restauration = true
+      college.save!
+    end
+
+    if row['Hebergement'] == 1
+      college.has_hebergement = true
+      college.save!
+    end
+
+    if row['ULIS'] == 1
+      college.has_ulis = true
+      college.save!
+    end
+
+    if row['Apprentissage'] == 1
+      college.has_apprentissage = true
+      college.save!
+    end
+
+    if row['Segpa'] == 1
+      college.has_segpa = true
+      college.save!
+    end
+
+    if row['Section_arts'] == 1
+      college.has_art_section = true
+      college.save!
+    end
+
+    if row['Section_cinema'] == 1
+      college.has_cinema_section = true
+      college.save!
+    end
+
+    if row['Section_theatre'] == 1
+      college.has_theatre_section = true
+      college.save!
+    end
+
+    if row['Section_sport'] == 1
+      college.has_sport_section = true
+      college.save!
+    end
+
+    if row['Section_internationale'] == 1
+      college.has_international_section = true
+      college.save!
+    end
+
+    if row['Section_europeenne'] == 1
+      college.has_european_section = true
+      college.save!
+    end
+
+    if row['Lycee_Agricole'] == 1
+      college.has_agricole_section = true
+      college.save!
+    end
+
+    if row['Lycee_militaire'] == 1
+      college.has_military_section = true
+      college.save!
+    end
+
+    if row['Lycee_des_metiers'] == 1
+      college.has_metiers_section = true
+      college.save!
+    end
+
+    if row['Post_BAC'] == 1
+      college.has_post_bac_sections = true
+      college.save!
+    end
+
+    if (row['Appartenance_Education_Prioritaire'] != "") && (row['Appartenance_Education_Prioritaire'] != nil)
+      college.is_educ_priority_zone = true
+      college.save!
+    end
+    # lycee.rating = (1..5).to_a.sample
+    # lycee.save!
+  end
+end
+
+
+puts "fr-en-annuaire-education csv file parsed successfully !!!!"
+
+
+
+filepath = 'db/lyceecollegefrance.csv'
+
+puts "Parsing lyceecollegefrance csv file ..."
+CSV.foreach(filepath, csv_options) do |row|
+  if College.find_by(uai: row["code UAI"][0...8])
+    college = College.find_by(uai: row["code UAI"][0...8])
+    college.departement = row['département']
+    college.academie = row['académie']
+    college.region = row['région']
+    college.latitude = row['latitude (Y)']
+    college.longitude = row['longitude (X)']
+    college.save!
+
+  elsif Lycee.find_by(uai: row["code UAI"][0...8])
+    lycee = Lycee.find_by(uai: row["code UAI"][0...8])
+    lycee.departement = row['département']
+    lycee.academie = row['académie']
+    lycee.region = row['région']
+    lycee.latitude = row['latitude (Y)']
+    lycee.longitude = row['longitude (X)']
     lycee.save!
   end
 end
+
+puts "Parsing lyceecollegefrance over succesfully ! "
+
